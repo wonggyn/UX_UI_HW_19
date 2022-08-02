b2d94c68f96a9a6a214256c4647a41faa34fdc0d
wonggyn <101224873+wonggyn@users.noreply.github.com>
2022-08-01 19:32:11 -0700

    Code update

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
 console.log("Your index.js file is loaded correctly!");
+
+$(document).ready(function() {
+    /*
+    $(".hover").hover(
+        function() {
+            $(this).css("text-decoration", "underline");
+            $(this).css("font-weight", "bold");
+        }, function() {
+            $(this).css("text-decoration", "inherit");
+            $(this).css("font-weight", "normal");
+    });
+    */
+
+    $(".hover").hover(
+        function() {
+            $(this).addClass("a2");
+        }, function() {
+            $(this).removeClass("a2");
+    });
+
+    $(".button").hover(
+        function() {
+            $(this).addClass("button2");
+        }, function() {
+            $(this).removeClass("button2");
+    });
+
+});
